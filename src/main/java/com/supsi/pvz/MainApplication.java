c6260084b823fbdffcd5f9a30625d07ffca0394e
ignacioUtrilla <utrillamas@gmail.com>
2022-03-22 08:26:46 +0000

    feat: visualizzazione griglia di gioco (#20142)
    Merge-request: PLANT-VS-ZOMBIE-MR-2
    Merged-by: Ignacio Utrilla Mas <utrillamas@gmail.com>

--- src/main/java/com/supsi/pvz/MainApplication.java
+++ src/main/java/com/supsi/pvz/MainApplication.java
@@ -11,8 +11,8 @@ public class MainApplication extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/main.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 800, 600);
     stage.setTitle("PvZ");
     stage.setScene(scene);
     stage.show();
--- src/main/java/com/supsi/pvz/MainController.java
+++ src/main/java/com/supsi/pvz/MainController.java
@@ -1,5 +1,15 @@
 package com.supsi.pvz;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class MainController {
 
+  @FXML
+  public GridPane gameGrid;
+
+  @FXML
+  public void initialize() {
+    gameGrid.setGridLinesVisible(true);
+  }
 }
