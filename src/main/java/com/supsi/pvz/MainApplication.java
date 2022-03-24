db34f8851efd4d9bc7d135c0c237fd6b8ff3e4d6
ignacioUtrilla <utrillamas@gmail.com>
2022-03-24 15:21:43 +0000

    feat: init resource bundle (#20143)
    Merge-request: PLANT-VS-ZOMBIE-MR-5
    Merged-by: Ignacio Utrilla Mas <utrillamas@gmail.com>

--- src/main/java/com/supsi/pvz/MainApplication.java
+++ src/main/java/com/supsi/pvz/MainApplication.java
@@ -7,14 +7,18 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class MainApplication extends Application {
 
   private static final String windowTitle = "PvZ";
+  private static final ResourceBundle rscBundle = ResourceBundle.getBundle("i18n/strings");
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/main-menu.fxml"));
+    Locale.setDefault(Locale.ENGLISH);
+    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/main-menu.fxml"), rscBundle);
     Scene scene = new Scene(fxmlLoader.load(), 800, 600);
     stage.setTitle(windowTitle);
     stage.setScene(scene);
@@ -22,7 +26,7 @@ public class MainApplication extends Application {
   }
 
   public static void changeScene(String fxml) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxml));
+    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxml), rscBundle);
     Stage stage = (Stage)Window.getWindows().get(0);
     Scene scene = new Scene(fxmlLoader.load(), stage.getScene().getWidth(), stage.getScene().getHeight());
     stage.setTitle(windowTitle);
