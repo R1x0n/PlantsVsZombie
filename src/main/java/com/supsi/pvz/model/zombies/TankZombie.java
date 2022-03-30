57dae28a9dba5418f668b2b9e670df4db208c4c9
Dominik Ferraro <dominik.ferraro@assetmax.ch>
2022-03-30 20:19:22 +0000

    feat: zombies implementation (#20068)
    Merge-request: PLANT-VS-ZOMBIE-MR-8
    Merged-by: Dominik Ferraro

---
+++ src/main/java/com/supsi/pvz/model/zombies/TankZombie.java
@@ -0,0 +1,9 @@
+package com.supsi.pvz.model.zombies;
+
+public class TankZombie extends AbstractZombie {
+
+    public TankZombie() {
+        super(200, 1, 0.5);
+    }
+
+}
---
+++ src/test/ZombiesTest.java
@@ -0,0 +1,18 @@
+import com.supsi.pvz.model.zombies.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ZombiesTest {
+
+    @Test
+    public void takeDamageTest() {
+        NormalZombie nz = new NormalZombie();
+        TankZombie tz = new TankZombie();
+        RunnerZombie rz = new RunnerZombie();
+
+        assertEquals(80, nz.takeDamage(20));
+        assertEquals(180, tz.takeDamage(20));
+        assertEquals(30, rz.takeDamage(20));
+    }
+}
