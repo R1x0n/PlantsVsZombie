0eaabc42dcc23735e98ee4a01148782fc98b4491
Dominik Ferraro <dominik.ferraro@assetmax.ch>
2022-05-20 14:04:34 +0000

    feat: empty_cell_check (#20395) Merge-request: PLANT-VS-ZOMBIE-MR-29 Merged-by: Dominik Ferraro

--- frontend/src/main/java/com/supsi/frontend/components/gameGrid/CellComponent.java
+++ frontend/src/main/java/com/supsi/frontend/components/gameGrid/CellComponent.java
@@ -3,26 +3,30 @@ package com.supsi.frontend.components.gameGrid;
 import com.almasb.fxgl.entity.component.Component;
 import com.supsi.backend.observers.Points;
 import com.supsi.backend.observers.SelectedPlant;
+import com.supsi.backend.observers.utils.Observer;
 import com.supsi.frontend.components.plant.PlantComponent;
 
+import javafx.animation.FadeTransition;
+import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.spawn;
 
-public class CellComponent extends Component {
+public class CellComponent extends Component implements Observer {
     private static final SelectedPlant<PlantComponent<Rectangle>> selectedPlant = SelectedPlant.getInstance();
     private final Points points = Points.getInstance();
+    private Rectangle shape;
+    private Point2D cellCenter;
+    private FadeTransition ft;
 
     private void onClick(MouseEvent event) {
         var plant = selectedPlant.getState();
-        if (plant != null) {
-            double cellWidth = GridComponent.getCellWidth() / 2;
-            double cellHeight = GridComponent.getCellHeight() / 2;
-            double x = entity.getX() + cellWidth - plant.getWidth() / 2;
-            double y = entity.getY() + cellHeight - plant.getHeight() / 2;
-            spawn(plant.getFactoryId(), x, y);
+        if (plant != null && getGameWorld().getEntitiesAt(cellCenter).size() == 0) {
+            spawn(plant.getFactoryId(), cellCenter.getX(), cellCenter.getY());
             points.remove(plant.getPlant().getPrice());
             selectedPlant.setState(null);
         }
@@ -30,8 +34,34 @@ public class CellComponent extends Component {
 
     @Override
     public void onAdded() {
-        var shape = new Rectangle(GridComponent.getCellWidth(), GridComponent.getCellHeight(), Color.TRANSPARENT);
+        var plant = selectedPlant.getState();
+        double cellWidth = GridComponent.getCellWidth() / 2;
+        double cellHeight = GridComponent.getCellHeight() / 2;
+        cellCenter = new Point2D(entity.getX() + cellWidth - plant.getWidth() / 2, entity.getY() + cellHeight - plant.getHeight() / 2);
+        shape = new Rectangle(GridComponent.getCellWidth(), GridComponent.getCellHeight(), Color.WHITE);
+        shape.setOpacity(0);
+        configureFadeTransition();
+        selectedPlant.attach(this);
         entity.getViewComponent().addChild(shape);
         entity.getViewComponent().addEventHandler(MouseEvent.MOUSE_CLICKED, this::onClick);
+    }
+
+    @Override
+    public void update() {
+        if (selectedPlant.getState() != null && getGameWorld().getEntitiesAt(cellCenter).size() == 0) {
+            shape.setFill(Color.WHITE);
+            ft.play();
+        } else {
+            shape.setOpacity(0);
+            ft.stop();
+        }
+    }
+
+    private void configureFadeTransition() {
+        ft = new FadeTransition(Duration.millis(1000), shape);
+        ft.setFromValue(0);
+        ft.setToValue(0.1);
+        ft.setCycleCount(Integer.MAX_VALUE);
+        ft.setAutoReverse(true);
     }
 }
